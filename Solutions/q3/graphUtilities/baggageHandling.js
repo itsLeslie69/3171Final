5375e7057f387cc62f26d9f1a55bd6cca481f020
makayla5 <86327828+makayla5@users.noreply.github.com>
2024-11-25 06:13:33 -0500

    fixing directories

--- Solutions/q3/graphUtilities/baggageHandling.js
+++ Solutions/q3/graphUtilities/baggageHandling.js
@@ -1,7 +1,20 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm"
 
+  //fetching customer_satisfaction.csv
+      //making sure our directory path is correct
+      const csvPath = "../../data/customer_satisfaction.csv";
+      fetch(csvPath)
+        .then((response) => {
+          if (!response.ok)
+            throw new Error(`Failed to fetch FOR baggagehandling.js: ${response.statusText}`);
+          return response.text();
+        })
+        .then((data) => console.log("baggagehandling.js csv data successfully recieved: " + data))
+        .catch((error) => console.error(error));
+
+
 export default async function returnBaggageHandling () {
-    const data = await d3.csv('/data/customer_satisfaction.csv');
+    const data = await d3.csv('../../../data/customer_satisfaction.csv');
 
     const oneStar = data.filter((record) => record["Baggage handling"] == 1).length;
     const twoStar = data.filter((record) => record["Baggage handling"] == 2).length;
--- Solutions/q3/graphUtilities/booking.js
+++ Solutions/q3/graphUtilities/booking.js
@@ -1,7 +1,20 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm"
 
+  //fetching customer_satisfaction.csv
+      //making sure our directory path is correct
+      const csvPath = "../../data/customer_satisfaction.csv";
+      fetch(csvPath)
+        .then((response) => {
+          if (!response.ok)
+            throw new Error(`Failed to fetch FOR booking.js: ${response.statusText}`);
+          return response.text();
+        })
+        .then((data) => console.log("booking.js csv data successfully recieved: " + data))
+        .catch((error) => console.error(error));
+
+
 export default async function returnBooking () {
-    const data = await d3.csv('/data/customer_satisfaction.csv');
+    const data = await d3.csv('../../../data/customer_satisfaction.csv');
 
     const oneStar = data.filter((record) => record["Ease of Online booking"] == 1).length;
     const twoStar = data.filter((record) => record["Ease of Online booking"] == 2).length;
