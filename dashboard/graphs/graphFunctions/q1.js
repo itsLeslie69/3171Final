992eb9af757b9b145737ccf5ec866f245ef584d0
makayla5 <86327828+makayla5@users.noreply.github.com>
2024-12-02 20:57:38 -0500

    added zoomable graph to dashboard with tooltips that work still needs to be consistent with the color scheme and UI of other graph theme

--- dashboard/graphs/graphFunctions/q1.js
+++ dashboard/graphs/graphFunctions/q1.js
@@ -1,6 +1,7 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-export default function getGraph1() {
+export default function renderFlightDistanceChart() {
+  //set dimensions and margins
   const margin = { top: 20, right: 30, bottom: 50, left: 100 };
   const width = 1000 - margin.left - margin.right;
   const height = 500 - margin.top - margin.bottom;
@@ -11,24 +12,16 @@ export default function getGraph1() {
   const xAxis = d3.axisBottom(xScale);
   const yAxis = d3.axisLeft(yScale);
 
-  // Select container and create SVG
   const svg = d3
-    .select("#chart-container")
+    .select("#q1Container")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
-
-  const axesGroup = svg
+    .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  const chartArea = svg
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`)
-    .attr("clip-path", "url(#clip)");
-
-  // Add clipping path
-  chartArea
+  //add a clipping path
+  svg
     .append("defs")
     .append("clipPath")
     .attr("id", "clip")
@@ -36,37 +29,50 @@ export default function getGraph1() {
     .attr("width", width)
     .attr("height", height);
 
-  // Axes labels
-  axesGroup
+  //add X-axis title
+  svg
     .append("text")
+    .attr("id", "x-axis-title")
     .attr("x", width / 2)
     .attr("y", height + margin.bottom - 10)
     .attr("text-anchor", "middle")
     .attr("font-size", "16px")
+    .attr("fill", "black")
     .text("Flight ID");
 
-  axesGroup
+  //add Y-axis title
+  svg
     .append("text")
+    .attr("id", "y-axis-title")
     .attr("transform", "rotate(-90)")
     .attr("x", -height / 2)
     .attr("y", -margin.left + 20)
     .attr("text-anchor", "middle")
     .attr("font-size", "16px")
+    .attr("fill", "black")
     .text("Flight Distance");
 
-  // Tooltip
+  svg.append("g").attr("class", "x axis").attr("transform", `translate(0,${height})`);
+  svg.append("g").attr("class", "y axis");
+
+  const line = d3
+    .line()
+    .x((d) => xScale(d.id))
+    .y((d) => yScale(d.distance));
+
+  //tooltip container
   const tooltip = d3
     .select("body")
     .append("div")
     .attr("class", "tooltip")
     .style("position", "absolute")
-    .style("background", "#fff")
-    .style("border", "1px solid #ccc")
-    .style("padding", "5px")
+    .style("background-color", "white")
+    .style("border", "1px solid black")
     .style("border-radius", "5px")
+    .style("padding", "10px")
+    .style("pointer-events", "none")
     .style("opacity", 0);
 
-  // Load data
   d3.csv("../data/customer_satisfaction.csv").then((data) => {
     const parsedData = data.map((d, i) => ({
       id: i,
@@ -77,30 +83,35 @@ export default function getGraph1() {
     const satisfiedData = parsedData.filter((d) => d.satisfied);
     const notSatisfiedData = parsedData.filter((d) => !d.satisfied);
 
-    // Set domains
     xScale.domain([0, parsedData.length + 1]);
     yScale.domain([0, d3.max(parsedData, (d) => d.distance)]);
 
-    // Satisfied line
-    chartArea
+    //satisfied line
+    svg
+      .append("g")
+      .attr("clip-path", "url(#clip)")
       .append("path")
       .datum(satisfiedData)
       .attr("class", "line satisfied")
-      .attr("d", d3.line().x((d) => xScale(d.id)).y((d) => yScale(d.distance)))
+      .attr("d", line)
       .attr("stroke", "green")
       .attr("fill", "none");
 
-    // Not satisfied line
-    chartArea
+    //not satisfied line
+    svg
+      .append("g")
+      .attr("clip-path", "url(#clip)")
       .append("path")
       .datum(notSatisfiedData)
       .attr("class", "line not-satisfied")
-      .attr("d", d3.line().x((d) => xScale(d.id)).y((d) => yScale(d.distance)))
+      .attr("d", line)
       .attr("stroke", "red")
       .attr("fill", "none");
 
-    // Points
-    chartArea
+    //satisfied points
+    svg
+      .append("g")
+      .attr("clip-path", "url(#clip)")
       .selectAll(".dot-satisfied")
       .data(satisfiedData)
       .join("circle")
@@ -109,21 +120,30 @@ export default function getGraph1() {
       .attr("cy", (d) => yScale(d.distance))
       .attr("r", 5)
       .attr("fill", "green")
-      .on("mouseover", (event, d) => {
+      .on("mouseover", function (event, d) {
         tooltip
           .style("opacity", 1)
-          .html(`ID: ${d.id}<br>Distance: ${d.distance}<br>Status: Satisfied`)
+          .html(
+            `<strong>Flight ID:</strong> ${d.id}<br>
+            <strong>Distance:</strong> ${d.distance}<br>
+            <strong>Status:</strong> Satisfied`
+          )
           .style("left", `${event.pageX + 10}px`)
           .style("top", `${event.pageY - 10}px`);
+        d3.select(this).attr("r", 8).attr("fill", "blue");
       })
-      .on("mousemove", (event) => {
+      .on("mousemove", function (event) {
         tooltip.style("left", `${event.pageX + 10}px`).style("top", `${event.pageY - 10}px`);
       })
-      .on("mouseout", () => {
+      .on("mouseout", function () {
         tooltip.style("opacity", 0);
+        d3.select(this).attr("r", 5).attr("fill", "green");
       });
 
-    chartArea
+    //not satisfied points
+    svg
+      .append("g")
+      .attr("clip-path", "url(#clip)")
       .selectAll(".dot-not-satisfied")
       .data(notSatisfiedData)
       .join("circle")
@@ -132,66 +152,27 @@ export default function getGraph1() {
       .attr("cy", (d) => yScale(d.distance))
       .attr("r", 5)
       .attr("fill", "red")
-      .on("mouseover", (event, d) => {
+      .on("mouseover", function (event, d) {
         tooltip
           .style("opacity", 1)
-          .html(`ID: ${d.id}<br>Distance: ${d.distance}<br>Status: Not Satisfied`)
+          .html(
+            `<strong>Flight ID:</strong> ${d.id}<br>
+            <strong>Distance:</strong> ${d.distance}<br>
+            <strong>Status:</strong> Not Satisfied`
+          )
           .style("left", `${event.pageX + 10}px`)
           .style("top", `${event.pageY - 10}px`);
+        d3.select(this).attr("r", 8).attr("fill", "blue");
       })
-      .on("mousemove", (event) => {
+      .on("mousemove", function (event) {
         tooltip.style("left", `${event.pageX + 10}px`).style("top", `${event.pageY - 10}px`);
       })
-      .on("mouseout", () => {
+      .on("mouseout", function () {
         tooltip.style("opacity", 0);
+        d3.select(this).attr("r", 5).attr("fill", "red");
       });
 
-    // Render axes
-    axesGroup.select(".x.axis").call(xAxis);
-    axesGroup.select(".y.axis").call(yAxis);
-
-    // Add zoom
-    const zoom = d3
-      .zoom()
-      .scaleExtent([1, 10])
-      .translateExtent([
-        [0, 0],
-        [width, height],
-      ])
-      .on("zoom", zoomed);
-
-    chartArea
-      .append("rect")
-      .attr("width", width)
-      .attr("height", height)
-      .style("fill", "none")
-      .style("pointer-events", "all")
-      .call(zoom);
-
-    function zoomed({ transform }) {
-      const newXScale = transform.rescaleX(xScale);
-
-      axesGroup.select(".x.axis").call(xAxis.scale(newXScale));
-
-      chartArea.selectAll(".line.satisfied").attr(
-        "d",
-        d3.line().x((d) => newXScale(d.id)).y((d) => yScale(d.distance))
-      );
-
-      chartArea.selectAll(".line.not-satisfied").attr(
-        "d",
-        d3.line().x((d) => newXScale(d.id)).y((d) => yScale(d.distance))
-      );
-
-      chartArea
-        .selectAll(".dot-satisfied")
-        .attr("cx", (d) => newXScale(d.id))
-        .attr("cy", (d) => yScale(d.distance));
-
-      chartArea
-        .selectAll(".dot-not-satisfied")
-        .attr("cx", (d) => newXScale(d.id))
-        .attr("cy", (d) => yScale(d.distance));
-    }
+    svg.select(".x.axis").call(xAxis);
+    svg.select(".y.axis").call(yAxis);
   });
 }
